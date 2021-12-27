41ae2dac6a617188641485fad75285287cc0ddaa
eisandbar <55670245+eisandbar@users.noreply.github.com>
2021-12-27 16:36:30 +0300

    Using new packages for routing. Changed host to 0.0.0.0

--- src/server/main.go
+++ src/server/main.go
@@ -5,21 +5,29 @@ import (
     "net/http"
     "io"
     "log"
+    "github.com/gorilla/mux"
+    "github.com/rs/cors"
     sep "polarion/network/src/server/endpoints"
 )
 
 const (
-    CONN_HOST = "server"
+    CONN_HOST = "0.0.0.0"
     CONN_PORT = "3333"
     CONN_TYPE = "tcp"
 )
 
 func main() {
-    http.HandleFunc("/ping", sep.Ping)
-    http.HandleFunc("/longQ", sep.LongQuery)
+    router := mux.NewRouter()
+
+    router.HandleFunc("/ping", sep.Ping).Methods("GET")
+    router.HandleFunc("/longQ", sep.LongQuery).Methods("GET")
+    router.HandleFunc("/pgQ/{id}", sep.PGQuery).Methods("GET")
+    
+    handler := cors.Default().Handler(router)
     
     fmt.Println("Listening on port:", CONN_PORT)
-    log.Fatal(http.ListenAndServe(CONN_HOST + ":" + CONN_PORT, nil))
+
+    log.Fatal(http.ListenAndServe(CONN_HOST + ":" + CONN_PORT, handler))
 }
 
 // Handles incoming requests.
