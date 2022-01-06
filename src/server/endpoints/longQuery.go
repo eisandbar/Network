eb07fe0c17259ad775678c2f5326316d9062c9d8
eisandbar <55670245+eisandbar@users.noreply.github.com>
2022-01-06 18:18:37 +0300

    Changed print statements

--- src/server/endpoints/longQuery.go
+++ src/server/endpoints/longQuery.go
@@ -8,7 +8,9 @@ import (
 )
 
 func LongQuery (w http.ResponseWriter, _ *http.Request) {
+	
+	fmt.Println("Received long query")
 	time.Sleep(20 * time.Second)
 	io.WriteString(w, "This query takes 20s\n")
-	fmt.Println("Received long query")
+	fmt.Println("Waited 20s")
 }
