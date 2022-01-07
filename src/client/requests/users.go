46f2a955706ab42deb031fb850a69b8a96342801
eisandbar <55670245+eisandbar@users.noreply.github.com>
2022-01-08 01:23:03 +0300

    Added queries for the rest API. The Idea is to have a lot of reads to overload the db, which can be fixed by adding a cache

---
+++ src/client/requests/users.go
@@ -0,0 +1,54 @@
+package requests
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"io"
+	"bytes"
+	"encoding/json"
+	"math/rand"
+	conn "polarion/network/src/client/util"
+)
+
+type User struct {
+	Username string
+	Email string
+	Password string
+	Id int64
+	ProfileId int64
+}
+
+func UserPost () {
+	newUser := Constructor()
+	b, err := json.Marshal(newUser)
+	// Post request
+	resp, err := http.Post(conn.ConnStr + "/users", "application/json", bytes.NewBuffer(b))
+	if err != nil {
+		log.Fatalf("Error Connecting: %v", err)
+	}
+	// Close body when the application closes.
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+
+	if resp.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", resp.StatusCode, body)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", body)
+}
+
+func Constructor () User {
+	var u User
+	l := 5 + rand.Intn(12)
+	username := make([]byte, l)
+	for i:=0; i<l; i++ {
+		username[i] = byte(rand.Intn(26)) + byte('a')
+	}
+	u.Username = string(username)
+	u.Email = u.Username + "@mail.com"
+	u.Password = "secretPass"
+	return u
+}
