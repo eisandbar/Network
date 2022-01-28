6d8f5120caa3485d65f90cfb3a5dc14b0e141b18
eisandbar <eisandbar@gmail.com>
2022-01-28 21:31:09 +0300

    Worker servers listen on load_query_rpc queue and returns the corresponding fibonacci numbers. This is done to mimic a cpu consuming task.

--- src/worker/main.go
+++ src/worker/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
         "log"
-        "os"
+        "strconv"
+        "fmt"
         "github.com/streadway/amqp"
 )
 
 func failOnError(err error, msg string) {
         if err != nil {
                 log.Fatalf("%s: %s", msg, err)
+        }
+}
+
+func fib(n int) int {
+        if n == 0 {
+                return 0
+        } else if n == 1 {
+                return 1
+        } else {
+                return (fib(n-1) + fib(n-2)) % 10037
         }
 }
 
@@ -21,62 +32,62 @@ func main() {
         failOnError(err, "Failed to open a channel")
         defer ch.Close()
 
-        err = ch.ExchangeDeclare(
-                "logs_topic", // name
-                "topic",      // type
-                true,         // durable
-                false,        // auto-deleted
-                false,        // internal
-                false,        // no-wait
-                nil,          // arguments
-        )
-        failOnError(err, "Failed to declare an exchange")
-
         q, err := ch.QueueDeclare(
-                "",    // name
-                false, // durable
-                false, // delete when unused
-                true,  // exclusive
-                false, // no-wait
-                nil,   // arguments
+                "load_query_rpc", // name
+                false,       // durable
+                false,       // delete when unused
+                false,       // exclusive
+                false,       // no-wait
+                nil,         // arguments
         )
         failOnError(err, "Failed to declare a queue")
 
-        if len(os.Args) < 2 {
-                log.Printf("Usage: %s [binding_key]...", os.Args[0])
-                os.Exit(0)
-        }
-        for _, s := range os.Args[1:] {
-                log.Printf("Binding queue %s to exchange %s with routing key %s",
-                        q.Name, "logs_topic", s)
-                err = ch.QueueBind(
-                        q.Name,       // queue name
-                        s,            // routing key
-                        "logs_topic", // exchange
-                        false,
-                        nil)
-                failOnError(err, "Failed to bind a queue")
-        }
+        err = ch.Qos(
+                1,     // prefetch count
+                0,     // prefetch size
+                false, // global
+        )
+        failOnError(err, "Failed to set QoS")
 
         msgs, err := ch.Consume(
                 q.Name, // queue
                 "",     // consumer
-                true,   // auto ack
+                false,  // auto-ack
                 false,  // exclusive
-                false,  // no local
-                false,  // no wait
+                false,  // no-local
+                false,  // no-wait
                 nil,    // args
         )
         failOnError(err, "Failed to register a consumer")
 
-        forever := make(chan bool)
+        fmt.Println("Connected to rabbitmq")
+
+        forever := make(chan bool) // used to keep listening
 
         go func() {
                 for d := range msgs {
-                        log.Printf(" [x] %s", d.Body)
+                        n, err := strconv.Atoi(string(d.Body))
+                        failOnError(err, "Failed to convert body to integer")
+
+                        log.Printf(" [.] fib(%d)", n)
+                        response := fib(n)
+
+                        err = ch.Publish(
+                                "",        // exchange
+                                d.ReplyTo, // routing key
+                                false,     // mandatory
+                                false,     // immediate
+                                amqp.Publishing{
+                                        ContentType:   "text/plain",
+                                        CorrelationId: d.CorrelationId,
+                                        Body:          []byte(strconv.Itoa(response)),
+                                })
+                        failOnError(err, "Failed to publish a message")
+
+                        d.Ack(false)
                 }
         }()
 
-        log.Printf(" [*] Waiting for logs. To exit press CTRL+C")
+        log.Printf(" [*] Awaiting RPC requests")
         <-forever
 }
